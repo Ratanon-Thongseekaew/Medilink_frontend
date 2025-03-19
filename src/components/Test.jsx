5d4117aa22f8a657be8797efa4187f73a94a0bcd
naraphan13 <thongnit2201@gmail.com>
2025-03-19 16:50:05 +0700

    start payment

---
+++ src/components/Test.jsx
@@ -0,0 +1,21 @@
+import axios from 'axios'
+import React from 'react'
+
+export const checkOut = async (token, id) => {
+    return await axios.post("http://localhost:8888/api/program/checkout",
+        { id },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+}
+
+export const checkOutStatus = async (token, session) => {
+    return await axios.get(`http://localhost:8888/api/program/checkout-status/${session}`,
+        {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+}
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -22,6 +22,8 @@ import HospitalProfile from "../pages/Admin/hospital/hospitalProfile";
 import ThankyouAppointment from "../pages/ThankyouAppointment";
 import Package from "../pages/Package";
 import ThankyouPackage from "../pages/ThankyouPackage";
+import Checkout from "../components/Checkout";
+import CheckoutComplete from "../components/CheckoutComplete";
 
 const guestRouter = createBrowserRouter([
     {path: "/", element: <Home />},
@@ -38,6 +40,8 @@ const userRouter = createBrowserRouter([
         {path: "/appointment/:id", element: <Appointment />},
         {path: "/thankyou-appointment", element: <ThankyouAppointment />},
         {path: "/thankyou-package", element: <ThankyouPackage />},
+        {path: "/checkout", element: <Checkout />},
+        {path: "/checkout-complete/:session", element: <CheckoutComplete />},
         {path: "/package/:id", element: <Package />},
         {path: "*", element: <Navigate to="/" />}
        ]
